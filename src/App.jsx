23e3bea0c24584df0f2c75dffafc62c4734c7beb
Mariem Khaled <mariamkhaled709@gmail.com>
2023-02-06 03:09:18 +0300

    hope its last

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,24 @@ import { useState,useEffect } from 'react'
 function App() {
   const [products, setProducts] = useState([]);
   const[page,setPage]=useState(1);
+  const[totalPages,setTotalPages]=useState(0);
+
   useEffect(()=>{
-    fetch('https://dummyjson.com/products?limit=100')
+    fetch(`https://dummyjson.com/products?limit=10&skip=${page*5}`)
     .then(res => res.json())
     .then(data=>{
-      if(data&&data.products.length>0)setProducts(data.products)
-      
+      if(data&&data.products.length>0){
+        setProducts(data.products)
+        setTotalPages(data.total/5)
+      }
     console.log(data)
     });
-  },[])
+  },[page])
 
   return (
     <main>
       <div className="products">
-        {products.slice(page*10-10,page*10).map((p)=>{
+        {products.map((p)=>{
           return <span key={p.id} className="single--products">
                    <img src={p.thumbnail} alt={p.title}/>
                    <span>{p.title}</span>
@@ -29,18 +33,16 @@ function App() {
         <span 
         className={page<=1?"disable":""}
         onClick={()=>setPage(page-1)}>Prev</span>
-        {[...new Array(products.length/5)].map((_,i)=>{
+        {[...new Array(totalPages)].map((_,i)=>{
             return <span key={i} onClick={()=>{
-              setPage(i+1)
-              console.log(i+1,page)
-            }} 
+              setPage(i+1)}} 
           className={`pagination--values ${page===i+1?"selected":""}`}
             >{i+1}</span>;
         })}
         <span onClick={()=>{
             setPage(page+1)
         }}
-        className={page>=products.length/10?"disable":""}
+        className={page>=totalPages?"disable":""}
         >Next</span>
       </div>
     </main>
